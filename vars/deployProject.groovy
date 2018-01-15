d747fac6f66f76cb0b6295de492979e5dcab388f
MrChencc <34539085+MrChencc@users.noreply.github.com>
2018-01-15 11:28:26 +0800

    Update deployProject.groovy

--- vars/deployProject.groovy
+++ vars/deployProject.groovy
@@ -6,13 +6,14 @@ def call(body) {
   body.delegate = config
   body()
 
-  //def projectName = config.stagedProject[0]
-  //def releaseVersion = config.stagedProject[1]
+  def projectName = config.stagedProject[0]
+  def releaseVersion = config.stagedProject[1]
 
   //comment out as we're not using helm charts
   //unstash name:"staged-${projectName}-${releaseVersion}".hashCode().toString()
 
   def rc = readFile config.resourceLocation
+  sh 'echo config.environment'
 
   kubernetesApply(file: rc, environment: config.environment, registry: "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}")
 
